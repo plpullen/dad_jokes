7060e5fb8a00f58a1eb88315a46a401aec046df8
Peter Pullen <plpullen@gmail.com>
2022-03-12 14:19:53 -0500

    modified to use asynch/await

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ let theLoop
 //make function call generateJoke
 generateJoke()
 
-function generateJoke(){
+//with asynch await
+async function generateJoke(){
     const config = {
         headers: {'Accept': 'application/json'}
     }
-    fetch('https://icanhazdadjoke.com/', config)
-    .then(response => response.json())
-    .then(data => {
-        jokeEl.innerHTML = data.joke
-    })
+    const response = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
+
 }
+
+//USING .then()
+// function generateJoke(){
+//     const config = {
+//         headers: {'Accept': 'application/json'}
+//     }
+//     fetch('https://icanhazdadjoke.com/', config)
+//     .then(response => response.json())
+//     .then(data => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
 
 jokeBtn.addEventListener('click', generateJoke)
 loopJokeBtn.addEventListener('click', loopIt)
